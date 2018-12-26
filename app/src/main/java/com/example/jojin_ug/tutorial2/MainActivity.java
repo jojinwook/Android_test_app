c2d05d23392bcea37f12bbdea581a8c8eb2b8423
GeniusK <geniusk1047@naver.com>
2018-12-26 12:36:44 +0900

    코드 줄이기 - 1

--- app/src/main/java/com/example/jojin_ug/tutorial2/MainActivity.java
+++ app/src/main/java/com/example/jojin_ug/tutorial2/MainActivity.java
@@ -7,51 +7,40 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mNumber1;
+    private EditText mNumber2;
+    private TextView mResultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mNumber1 = findViewById(R.id.number1);
+        mNumber2 = findViewById(R.id.number2);
+        mResultView = findViewById(R.id.result);
     }
 
-    public void addClick(View v){
-        EditText number1 = (EditText) findViewById(R.id.number1);
-        EditText number2 = (EditText) findViewById(R.id.number2);
-        TextView result = (TextView) findViewById(R.id.result);
-        int n1 = Integer.parseInt(number1.getText().toString());
-        int n2 = Integer.parseInt(number2.getText().toString());
-        result.setText(Integer.toString(n1 + n2 ));
-
+    public void addClick(View v) {
+        int n1 = Integer.parseInt(mNumber1.getText().toString());
+        int n2 = Integer.parseInt(mNumber2.getText().toString());
+        mResultView.setText(Integer.toString(n1 + n2));
     }
 
-    public void subtrackClick(View v){
-        EditText number1 = (EditText) findViewById(R.id.number1);
-        EditText number2 = (EditText) findViewById(R.id.number2);
-        TextView result = (TextView) findViewById(R.id.result);
-        int n1 = Integer.parseInt(number1.getText().toString());
-        int n2 = Integer.parseInt(number2.getText().toString());
-        result.setText(Integer.toString(n1 - n2 ));
-
+    public void subtrackClick(View v) {
+        int n1 = Integer.parseInt(mNumber1.getText().toString());
+        int n2 = Integer.parseInt(mNumber2.getText().toString());
+        mResultView.setText(Integer.toString(n1 - n2));
     }
 
-    public void multiplyClick(View v){
-        EditText number1 = (EditText) findViewById(R.id.number1);
-        EditText number2 = (EditText) findViewById(R.id.number2);
-        TextView result = (TextView) findViewById(R.id.result);
-        int n1 = Integer.parseInt(number1.getText().toString());
-        int n2 = Integer.parseInt(number2.getText().toString());
-        result.setText(Integer.toString(n1 * n2 ));
-
+    public void multiplyClick(View v) {
+        int n1 = Integer.parseInt(mNumber1.getText().toString());
+        int n2 = Integer.parseInt(mNumber2.getText().toString());
+        mResultView.setText(Integer.toString(n1 * n2));
     }
 
-    public void divideClick(View v){
-        EditText number1 = (EditText) findViewById(R.id.number1);
-        EditText number2 = (EditText) findViewById(R.id.number2);
-        TextView result = (TextView) findViewById(R.id.result);
-        int n1 = Integer.parseInt(number1.getText().toString());
-        int n2 = Integer.parseInt(number2.getText().toString());
-        result.setText(Integer.toString(n1 / n2 ));
-
+    public void divideClick(View v) {
+        int n1 = Integer.parseInt(mNumber1.getText().toString());
+        int n2 = Integer.parseInt(mNumber2.getText().toString());
+        mResultView.setText(Integer.toString(n1 / n2));
     }
 }
